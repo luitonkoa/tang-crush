981a90cc2d4e7e106e1904ab27245455bc51956b
luitonkoa <87141848+luitonkoa@users.noreply.github.com>
2021-07-08 23:28:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Anh eo Hà Phương oiiiiii',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'hjhj',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy hê hê',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Emm biết mà 🥰. Yêu anh nhất nhất nhìu nhìuuuu',
+    messDesc: 'Tối nay 7h em qua đón nhé ông xãaaaaaa .',
+    btnAccept: 'Okeeeeeiiiii luaaaaan <3',
+    messLink: 'http://fb.com/100033850032873 
 }
